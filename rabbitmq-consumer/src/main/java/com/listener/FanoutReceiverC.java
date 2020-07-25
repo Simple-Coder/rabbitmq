7c4a189f3df634b0ee98199877a603b9bfaf4051
xiedong <dxiesjz@163.com>
2020-07-25 22:38:45 +0800

    fanout

---
+++ rabbitmq-consumer/src/main/java/com/listener/FanoutReceiverC.java
@@ -0,0 +1,23 @@
+package com.listener;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @ClassName: FanoutReceiverC
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/7/25 22:27
+ */
+@Component
+@RabbitListener(queues = "fanout.C")
+public class FanoutReceiverC {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("FanoutReceiverC消费者收到消息  : " + testMessage.toString());
+    }
+}
